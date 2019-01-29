53f3e321692d8598be6e7639e03829494c65af83
cdriesler <cdriesler.iv@gmail.com>
2019-01-29 14:03:21 -0500

    add loading effect for main page systems

--- public/src/app/systems/systems.component.ts
+++ public/src/app/systems/systems.component.ts
@@ -12,25 +12,50 @@ export class SystemsComponent implements OnInit {
   languages: Language[] = [];
 
   selectedDialect: DialectManifest;
+  selectedDialectName: string;
 
   constructor(private http:HttpClient) { }
 
-  onSelectDialect(selectedDialect:DialectManifest) {
-    this.selectedDialect = selectedDialect;
+  onSelectDialect(lang: Language, name:string) {
+    if (!this.isDialectLoaded(lang, name)) return;
+    
+    this.selectedDialectName = name;
+    this.languageContainsSelectedDialect(lang);
+  }
+
+  getDialectByName(lang:Language, name:string) : DialectManifest {
+    if (lang.dialects != undefined) {
+      return lang.dialects.find(x => x.name == name);
+    }
+    
+    return undefined;
   }
 
   languageContainsSelectedDialect(lang:Language) : boolean {
     let contains = false;
 
-    if (this.selectedDialect == undefined) { return false } 
+    if (this.selectedDialectName == undefined) { return false } 
 
     for (let dialect of lang.dialects) {
-      if (dialect.name == this.selectedDialect.name) {
+      if (dialect.name == this.selectedDialectName) {
+        this.selectedDialect = dialect;
         return true;
       }
     }
+    
+    return false;
+  }
+
+  isFirstDialect(lang:Language, dialectName:string) : boolean {
+    if (!this.isDialectLoaded(lang, dialectName)) return false;
+
+    if (lang.dialects[0].name == dialectName) return true;
 
     return false;
+  }
+
+  isDialectLoaded(lang:Language, dialectName:string) : boolean {
+    return lang.dialects.find(x => x.name == dialectName) != undefined;
   }
 
   ngOnInit() {
@@ -39,13 +64,14 @@ export class SystemsComponent implements OnInit {
     for (let language of res["languages"]) {
       let lang = new Language(language);
 
+      this.languages.push(lang);    
+
       for (let dialect of lang.dialectNames) {
         this.http.get("https://ourchitecture.app/api/box/" + lang.name + "/" + dialect).subscribe((result) => {
           lang.dialects.push(new DialectManifest(result));
+          console.log(dialect + " loaded")
         })
       }
-
-      this.languages.push(lang);
     }
     });
   }
